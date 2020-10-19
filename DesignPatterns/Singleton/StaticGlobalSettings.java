e3041251cb46b2d9a186bd789a3209d47ea55a54
YalingZheng <yaling.zheng@gmail.com>
2020-10-19 08:20:30 -0400

    add
    add

---
+++ DesignPatterns/Singleton/StaticGlobalSettings.java
@@ -0,0 +1,33 @@
+package singletonDPpackage;
+
+public class StaticGlobalSettings {
+	private static String serverip;
+	private static String defaultIP = "172.168.1.1";
+	private static String defaultPrefixIP = "172.168.1.";
+	
+	private StaticGlobalSettings(){
+		StaticGlobalSettings.serverip = defaultIP;
+	}
+	
+	private StaticGlobalSettings(String ip){
+		serverip = ip;
+	}	
+	
+	
+	public static StaticGlobalSettings getInstance() {
+		return new StaticGlobalSettings();
+	}
+	
+	public static String getServerIP() {
+		return serverip;
+	}
+
+	public synchronized static void setServerIP(String ip) {
+		serverip = ip;
+	}
+	
+	public static String getDefaultPrefixIP() {
+		return defaultPrefixIP;
+	}
+	
+}
